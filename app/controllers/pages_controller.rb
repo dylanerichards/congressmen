f5421bd90309199e30a6c8e63dbf54e4070843b7
Dylan Richards <dylanerichards@gmail.com>
2015-02-09 06:54:01 -0500

    Refactor domain logic into model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,12 @@
 class PagesController < ApplicationController
 
   def home
-    if user_signed_in? 
-    	Congress.key = '0ef8e130fe124b69a2388d253fd05e63'
-    	legislators = Congress.legislators_locate(current_user.zip)["results"]
-    	@house_rep = legislators.select{|leg| leg.chamber == "house"}[0]
-    	@senator1 = legislators.select{|leg| leg.chamber == "senate"}[0]
-    	@senator2 = legislators.select{|leg| leg.chamber == "senate"}[1]
+    if user_signed_in?
+      Congress.key = '0ef8e130fe124b69a2388d253fd05e63'
+
+      @house_rep = current_user.house_rep
+      @senator1 = current_user.senator1
+      @senator2 = current_user.senator2
     end
-  end
-
-  def about
-  end
-
-  def contact
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def house_rep
+    legislators.select { |legislator| legislator.chamber == "house" }[0]
+  end
+
+  def senator1
+    legislators.select { |legislator| legislator.chamber == "senate" }[0]
+  end
+
+  def senator2
+    legislators.select { |legislator| legislator.chamber == "house" }[1]
+  end
+
+  private
+
+  def legislators
+    Congress.legislators_locate(zip)["results"]
+  end
 end
